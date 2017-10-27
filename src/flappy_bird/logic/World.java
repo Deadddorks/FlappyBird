6f6aef6e9e6587fcb6e800208845afca970bced1
Kevin <ajfortcamp@bsu.edu>
2017-10-27 11:08:53 -0500

    take this bitch

--- src/flappy_bird/logic/World.java
+++ src/flappy_bird/logic/World.java
@@ -8,15 +8,28 @@ public class World
 {
 	
 	// ---------- Constants ----------
-	
+	private final double gravity = 10;
+	private final double scrollingSpeed = 10;
 	// ---------- --------- ----------
 	// ---------- Variables ----------
-	
+	private final double screenWidth;
+	private final double screenHieght;
+	private final double pipeWidth;
+	private ArrayList<Integer> pipes = new ArrayList<>();
+
+
 	// ---------- --------- ----------
 	
-	public World()
+	public World(final double screenWidth, final double screenHieght, final double pipeWidth)
 	{
-	
+		this.screenWidth = screenWidth;
+		this.screenHieght = screenHieght;
+		this.pipeWidth = pipeWidth;
 	}
-	
+
+	public void generatePipes()
+	{
+
+	}
+
 }
