3feb6d15f508f6dcccc07ca2cf95d6f8ebcb0077
Deadddorks <rudnicki.kalin@uwlax.edu>
2017-10-28 11:43:56 -0500

    Added a few ease of use

--- src/flappy_bird/graphics/Graphics.java
+++ src/flappy_bird/graphics/Graphics.java
@@ -5,6 +5,7 @@ import flappy_bird.logic.World;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 // ---------- ------- ----------
 
 public class Graphics
@@ -37,6 +38,9 @@ public class Graphics
 		Image background = new Image("resources/background.png");
 		
 		graphicsContext.drawImage(background, 0, 0, canvas.getWidth(), canvas.getHeight());
+		
+		drawPipesAtLoc(100, 400);
+		
 	}
 	
 	public World getWorld()
@@ -48,6 +52,12 @@ public class Graphics
 	{
 		world.gameTick();
 		draw();
+	}
+	
+	public void drawPipesAtLoc(final double xOffset, final double yOffset)
+	{
+		graphicsContext.drawImage(new Image("resources/pipe_up.png"), xOffset, yOffset);
+		graphicsContext.drawImage(new Image("resources/pipe_down.png"), xOffset, yOffset - PIPE_SEPARATION - canvas.getHeight());
 	}
 	
 	public void newGame()
